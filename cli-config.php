ec1c253934095399b57d444b83e8727aae890196
Marcelo Júnior <rokermarcelo@gmail.com>
2020-02-20 19:26:05 -0300

    Função de exclusão de curso

--- cli-config.php
+++ cli-config.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Alura\Cursos\Infra\EntityManagerCreator;
+
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    (new \Alura\Cursos\Infra\EntitymanagerCreator())->getEntityManager()
+    (new EntityManagerCreator())->getEntityManager()
 );
--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,12 @@
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\PersistirCurso;
+use Alura\Cursos\Controller\ExcluirCurso;
 
 return [
     '' => ListarCursos::class,
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso' => FormularioInsercao::class,
-    '/salvar-curso' => PersistirCurso::class
+    '/salvar-curso' => PersistirCurso::class,
+    '/excluir-curso' => ExcluirCurso::class,
 ];

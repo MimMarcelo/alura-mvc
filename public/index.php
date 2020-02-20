e854828184bfc7374486f8ed2182d6fe0aecf9dd
Marcelo Júnior <rokermarcelo@gmail.com>
2020-02-20 13:17:03 -0300

    Criada lógica de gravação de novo curso

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\PersistirCurso;
 
 switch ($_SERVER['PATH_INFO']) {
     case "":
@@ -14,6 +15,11 @@ switch ($_SERVER['PATH_INFO']) {
     
     case "/novo-curso":
         $controller = new FormularioInsercao();
+        $controller->processarRequisicao();
+        break;
+    
+    case "/salvar-curso":
+        $controller = new PersistirCurso();
         $controller->processarRequisicao();
         break;
     
